eaaf25213067eb021f38b72c805d3a8205f202bb
babita <babita.gurung@ultivic.com>
2023-01-17 15:41:44 +0530

    changes

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,7 +1,7 @@
 const Home=()=>{
  return (
         <div>
-           <h1>home pagssssssssssssssssse</h1>
+           <h1>home pagsssssssssssssssss welocme home</h1>
            <div class></div>
         </div>
     )
